b7e62948fb965651d247c5d8f07c8dab9e0ee71a
Codingwithriha <rihashehzadi2003@gmail.com>
2025-08-18 12:18:34 +0500

    changes

--- 03-modules.js
+++ 03-modules.js
@@ -1,8 +1,8 @@
 //Modules
 //CommonJS, every file is module(by default)
 //Modules - Encapsulated Code (only share minimum)
+const names = require('./04-names');
+const SayHi = require('./05-utils');
 
-
-
-SayHi('Riha')
-SayHi('Shahzadi')
+SayHi(names.riha);
+SayHi(names.shahzadi);
