95f993f668ed116c1717ddf81a76485016033da2
Codingwithriha <rihashehzadi2003@gmail.com>
2025-08-18 15:39:48 +0500

    less: 03-modules

--- 03-modules.js
+++ 03-modules.js
@@ -3,6 +3,7 @@
 //Modules - Encapsulated Code (only share minimum)
 const names = require('./04-names');
 const SayHi = require('./05-utils');
-
+const data = require('./06-alternative-flavour');
+require('./07-mind-grenade');
 SayHi(names.riha);
 SayHi(names.shahzadi);
